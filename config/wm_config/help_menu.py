2eef55899b6a46123f0d20e0fd8e753b1f739563
abhay-mohandas <abhay171005@gmail.com>
2022-07-08 11:25:40 +0530

    Added wallpapers and config changes

--- config/wm_config/help_menu.py
+++ config/wm_config/help_menu.py
@@ -4,28 +4,29 @@ os.system('clear')
 print("""
 [Note:Super key is same as Windows key]
 
-{Basic Commands}
-Close focused window:   [Super+C]
-Open Terminal:          [Super+Enter]
-Run Command:            [Super+R]
-Restart Gui:            [Super+Shift+R]
-Change Layout:          [Super+Tab]
-Change Focus:           [Super+Space]
-Wifi and Network:	[Super+Shift+W]
-(Keyboard only)
-
-{Brightness and Volume Control}
-Increase Brightness:    [Super+L]
-Decrease Brightness:    [Super+K]
-Increase Volume:        [Super+N]
-Decrease Volume:        [Super+B]
-Mute Volume:            [Super+M]
-
-
-{Power and Logout}
-Screen Lock:            [Super+Q]
-Logout/Power Menu:      [Super+Shift+Q]
-
+ {Basic Commands}                                   
+Close focused window:   [Super+C]                       Screenshot:         [Super+P]
+Open Terminal:          [Super+Enter]                   Fullscreen:         [Super+F]
+Run Command:            [Super+R]                       Floating mode:      [Super+Shift+F]
+Restart Gui:            [Super+Shift+R]                 Mouse Navigation:   [Super+<Right Click>] (Move the Window)
+Restart Config:         [Super+Shift+C]                                     [Super+<Left Click>]  (Resize the Window)
+Change Layout:          [Super+I] (Stacking mode)           
+                        [Super+O] (Tiling mode)     
+Change Focus:           [Super+<Arror Keys>]        
+Change Workspace:       [Super+<0-9>]               
+_________________________________________________________________________________________________________________________________________________________________________
+ {Brightness and Volume Control}                    |
+Increase Brightness:    [Super+L]                   |
+Decrease Brightness:    [Super+K]                   |
+Increase Volume:        [Super+N]                   |
+Decrease Volume:        [Super+B]                   |
+Mute Volume:            [Super+M]                   |
+                                                    |
+ {Power and Logout}                                 |
+Screen Lock:            [Super+Q]                   |
+Sleep:                  [Super+S]                   |
+Logout/Power Menu:      [Super+Shift+Q]             |
+                                                    |
 """)
 input("[Press Enter to exit]")
 os.system('clear')
--- config/wm_config/startup.sh
+++ config/wm_config/startup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
+#exec mpv --no-video .config/sound_effects/mixkit-magic-notification-ring-2344.wav &
 exec nitrogen --restore &
-exec picom --experimental-backend &
+exec picom &
+#exec picom --experimental-backend &
 #exec nm-applet &
 exec dunst &
 #exec alacritty &
